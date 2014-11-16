8059554b1652044310cc9fd9f845d6218625142a
Martin Crockett <martin.crockett@gmail.com>
2014-11-16 17:27:17 -0500

    Pins and users model updates

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   respond_to :html, :xml, :json
 
   def index
@@ -12,20 +14,28 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
-    respond_with(@pin)
+    @pin = current_user.pins.build
+    #@pin = Pin.new
+    #respond_with(@pin)
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
       render action: 'new'
     end
+
+    #@pin = Pin.new(pin_params)
+    #if @pin.save
+    #  redirect_to @pin, notice: 'Pin was successfully created.'
+    #else
+    #  render action: 'new'
+    #end
   end
 
   def update
@@ -46,7 +56,12 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
