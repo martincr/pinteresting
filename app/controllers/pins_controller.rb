f0d57bbeec3d92e4ad5339ba57a8ef05f7cec4b5
Martin Crockett <martin.crockett@gmail.com>
2014-11-16 15:39:07 -0500

    Cleaned routes, updated form

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -8,7 +8,7 @@ class PinsController < ApplicationController
   end
 
   def show
-    respond_with(@pin)
+    #respond_with(@pin)
   end
 
   def new
@@ -21,18 +21,24 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def update
-    @pin.update(pin_params)
-    respond_with(@pin)
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
     @pin.destroy
-    respond_with(@pin)
+    redirect_to pins_url
   end
 
   private
